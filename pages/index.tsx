afaa20fe6e61c4b6241f696cf6750ed4d1e7f9ef
Mikeultron <mikeul2.gt@gmail.com>
2022-08-20 02:54:27 +0700

    Update eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,25 @@ import { getPokemons } from "services/pokemon";
 
 const HomeView = dynamic(() => import("views/Home"));
 
-export default function HomePage() {
-  return <HomeView />;
-}
+const HomePage = () => {
+	return <HomeView />;
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const response = await getPokemons({});
-    return {
-      props: {
-        pokemons: response,
-      },
-    };
-  } catch (error: any) {
-    return {
-      props: {
-        pokemons: [],
-      },
-    };
-  }
+	try {
+		const response = await getPokemons({});
+		return {
+			props: {
+				pokemons: response,
+			},
+		};
+	} catch (error: any) {
+		return {
+			props: {
+				pokemons: [],
+			},
+		};
+	}
 };
+
+export default HomePage;
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import apiRequest from "./config";
 import { apiEndpoints } from "./url";
 
 const API = {
-  getPokemons: apiRequest.get(apiEndpoints.pokemon),
+	getPokemons: apiRequest.get(apiEndpoints.pokemon),
 };
 
 export default API;
